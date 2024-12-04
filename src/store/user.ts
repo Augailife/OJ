958ddefc82aeb619a21569e8c6b3c5d4962b3973
zhao <1098159607@qq.com>
2024-12-04 17:00:54 +0800

    根据接口文档生成前端axios

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,17 +1,25 @@
 import { StoreOptions } from "vuex";
 import accessEnum from "@/access/accessEnum";
+import { UserControllerService } from "../../generated";
 
 export default {
   namespaced: true,
   state: () => ({
     loginUser: {
       userName: "未登录",
-      role: accessEnum.NOT_LOGIN,
     },
   }),
   actions: {
-    getLoginUser({ commit }, payload) {
-      commit("updateLoginUser", payload);
+    async getLoginUser({ commit, state }, payload) {
+      const result = await UserControllerService.getLoginUserUsingGet();
+      if (result.code === 0) {
+        commit("updateLoginUser", result.data);
+      } else {
+        commit("updateLoginUser", {
+          ...state.loginUser,
+          role: accessEnum.NOT_LOGIN,
+        });
+      }
     },
   },
   mutations: {
